3a1f1e1b5d352988c660f948b6cc97b8aeeb549e
Dfreem <freemand@my.lanecc.edu>
2023-02-07 11:38:10 -0800

    --wip-- [skip ci]

---
+++ DevinSite/Data/SeedRoles.cs
@@ -0,0 +1,49 @@
+﻿namespace DevinSite.Data;
+
+public static class SeedRoles
+{
+    const string USER_NAME = "dfreem987";
+    const string PASSWORD = "!BassCase987";
+    const string ROLE_NAME = "Admin";
+
+    /// <summary>
+    /// Async method used in conjunction with Identity Role based authorization.
+    /// </summary>
+    /// <param name="services">a <see cref="IServiceProvider"/> that handles inject of dependencies.</param>
+    public static async Task SeedAdminUserAsync(IServiceProvider services)
+    {
+        RoleManager<IdentityRole> roleManager = services
+            .GetRequiredService<RoleManager<IdentityRole>>();
+
+        UserManager<Student> studentManager = services
+            .GetRequiredService<UserManager<Student>>();
+
+        // check for admin role existance
+        if (await roleManager.FindByNameAsync(ROLE_NAME) is null)
+        {
+            var roleStatus = await roleManager.CreateAsync(new IdentityRole(ROLE_NAME));
+            if (roleStatus.Succeeded) Console.WriteLine("Created Admin Role\nStatus: " + roleStatus.ToString());
+        }
+        if (await studentManager.FindByNameAsync(USER_NAME) is null)
+        {
+            // I'm the admin.
+            Student Devin = new()
+            {
+                UserName = USER_NAME,
+                Name = "Devin Freeman",
+                Email = "freemand@my.lanecc.edu",
+                EmailConfirmed = true,
+                RoleNames = new List<string>() { "Admin", "Student" },
+            };
+
+            //  administrator user creation with password
+            var status = await studentManager.CreateAsync(Devin, PASSWORD);
+
+            // add admin user to admin role.
+            if (status.Succeeded)
+            { await studentManager.AddToRoleAsync(Devin, ROLE_NAME); }
+        }
+    }
+
+}
+
--- DevinSite/ViewModels/ChangePasswordViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevinSite.ViewModels
-{
-    public class ChangePasswordViewModel
-    {
-        public string Username { get; set; } = default!;
-        public string OldPassword { get; internal set; } = default!;
-        public string NewPassword { get; internal set; } = default!;
-    }
-}
